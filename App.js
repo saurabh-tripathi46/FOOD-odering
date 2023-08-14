30fb9931798e42742b1426fb9a9d0531c3eeb6f6
Saurabh Tripathi <saurabh.t@zetwerk.com>
2023-08-15 00:47:37 +0530

    EP-1

---
+++ App.js
@@ -0,0 +1,8 @@
+const root = ReactDOM.createRoot(document.getElementById("root"))
+    const heading = React.createElement('div', {}, 
+    [React.createElement('div', {}, [React.createElement('h1', {}, 'sibling one')]), React.createElement('div', {}, [React.createElement('h2', {}, 'sibling two')])])
+    console.log(heading)
+    root.render(heading)
+
+    
+    // 
