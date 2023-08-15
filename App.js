73fe9c105122ccea7cbdf862340aa25bffb09fb0
Saurabh Tripathi <saurabh.t@zetwerk.com>
2023-08-15 19:28:23 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
     const heading = React.createElement('div', {}, 
     [React.createElement('div', {}, [React.createElement('h1', {}, 'sibling one')]), React.createElement('div', {}, [React.createElement('h2', {}, 'sibling two')])])
@@ -5,4 +8,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
     root.render(heading)
 
     
-    // 
+    
